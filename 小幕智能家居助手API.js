3a5aa19417a76fc8f2a2eac49f2bf2cee267729b
触手TV紫幕 <45619879+IZIMUI@users.noreply.github.com>
2020-01-29 21:28:30 +0800

    Update 小幕智能家居助手API.js

--- 小幕智能家居助手API.js
+++ 小幕智能家居助手API.js
@@ -1,5 +1,6 @@
 var GetUrl = window.location.href;
 var Start = GetUrl.indexOf("Search=");
+alert(Start)
 var End = GetUrl.indexOf("#");
 var Content = GetUrl.substring(Start+1,End);
 alert(Content);
